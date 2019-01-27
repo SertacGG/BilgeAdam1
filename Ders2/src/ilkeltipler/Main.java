980a6f07acc20de9c32726b3b274f39da5410bfe
sertac.guler <sertac8006@gmail.com>
2019-01-27 14:12:57 +0300

    ders 2

---
+++ Ders2/src/ilkeltipler/Main.java
@@ -0,0 +1,12 @@
+package ilkeltipler;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        System.out.println("a : " + a);
+
+    }
+
+}
---
+++ Ders2/src/tipdonusumleri/Main.java
@@ -0,0 +1,26 @@
+package tipdonusumleri;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+       /* byte bytea = 127;
+
+        int intb;
+        intb = bytea;
+        intb = 1000;
+        System.out.println("integer b : " + intb);
+
+        bytea = (byte) intb;
+        System.out.println("byte a 1000 olmalı ama veri kaybı oldu: " + bytea);*/
+
+        String xString;
+        Integer xInt = 12;
+
+        xString = xInt.toString();
+        System.out.println("String : " + xString);
+
+    }
+
+}
