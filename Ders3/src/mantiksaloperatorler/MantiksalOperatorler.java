8cebc84c7abd800beba703ce6a7c3a485591e5dd
sertac.guler <sertac8006@gmail.com>
2019-02-09 12:20:30 +0300

    ders 3

--- Ders3/src/mantiksaloperatorler/MantiksalOperatorler.java
+++ Ders3/src/mantiksaloperatorler/MantiksalOperatorler.java
@@ -4,6 +4,13 @@ public class MantiksalOperatorler {
 
     public static void main(String[] args) {
 
+        String sonuc = "";
+        int i;
+        for(i =0; i<=100; i++) {
+            if(i % 2 == 1) {
+                sonuc += i + " ";
+            }
+        }
+        System.out.println(sonuc);
     }
-
 }
