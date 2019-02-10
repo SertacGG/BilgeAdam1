1e723b987fbbb077e761f2f44c37bcae96beaa2c
sertac.guler <sertac8006@gmail.com>
2019-02-10 13:54:50 +0300

    calculator

---
+++ Metodlar/src/Foreachs.java
@@ -0,0 +1,10 @@
+public class Foreachs {
+
+    public static void main(String[] args) {
+        int[] dizi = {1,2,3,4,5,6,7,8,9};
+        for(int i:dizi) {
+            System.out.println(i);
+
+        }
+    }
+}
