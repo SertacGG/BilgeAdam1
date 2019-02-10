2fd9c5cd9ee930f78bb7286e037802a63b39162f
sertac.guler <sertac8006@gmail.com>
2019-02-10 11:15:26 +0300

    calculator

--- Metodlar/src/Ornekler.java
+++ Metodlar/src/Ornekler.java
@@ -1,31 +1,62 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Ornekler {
 
     public static void main(String[] args) {
-        helloMethod();
-        sayiYaz(52);
-        toplama(11,12);
-        int sonuc = carpma(12, 5);
-        System.out.println("Carpma sonucu : " + sonuc);
+
+        while (true){
+            islem();
+
+        }
 
     }
 
-    public static void helloMethod() {
-        System.out.println("Hello Method..");
+    public static void islem() {
+        Scanner input  = new Scanner(System.in);
+
+        System.out.println("Yapmak istediğiniz işlemi seçiniz");
+        System.out.println("1 - toplama");
+        System.out.println("2 - cikarma");
+        System.out.println("3 - carpma");
+        System.out.println("4 - bolme");
+        System.out.println("5 - mod");
+        String islem = input.nextLine();
+
+        System.out.println("ilk sayıyı girin");
+        int sayi1 = input.nextInt();
+        System.out.println("ikinci sayıyı girin");
+        int sayi2 = input.nextInt();
+        float sonuc ;
+
+        switch (islem) {
+            case "1" : sonuc = toplama(sayi1, sayi2); System.out.println(sayi1 + "+" + sayi2 + " = " + sonuc); break;
+            case "2" : sonuc = cikarma(sayi1, sayi2); System.out.println(sayi1 + "-" + sayi2 + " = " + sonuc); break;
+            case "3" : sonuc = carpma(sayi1, sayi2);  System.out.println(sayi1 + "*" + sayi2 + " = " + sonuc); break;
+            case "4" : sonuc = bolme(sayi1, sayi2);   System.out.println(sayi1 + "/" + sayi2 + " = " + sonuc); break;
+            case "5" : sonuc = mod(sayi1, sayi2);     System.out.println(sayi1 + "%" + sayi2 + " = " + sonuc); break;
+            default: System.out.println("işleminiz burada yok"); break;
+        }
+    }
+    public static int toplama(int sayi1, int sayi2) {
+        return sayi1 + sayi2 ;
 
     }
-
-    public static void sayiYaz(int sayi) {
-        System.out.println("Sayiniz : " + sayi);
-
-    }
-
-    public static void toplama(int sayi1, int sayi2) {
-        System.out.println("Toplam : " + (sayi1 + sayi2));
+    public static int cikarma(int sayi1, int sayi2) {
+        return sayi1 - sayi2 ;
 
     }
-
     public static int carpma(int sayi1, int sayi2) {
         return sayi1 * sayi2 ;
+
+    }
+    public static float bolme(int sayi1, int sayi2) {
+        float sonuc = (float) sayi1 / (float) sayi2;
+        return sonuc;
+
+    }
+    public static int mod(int sayi1, int sayi2) {
+        return sayi1 % sayi2 ;
 
     }
 
