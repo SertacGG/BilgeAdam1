d41e896efa5aa26567eee700c306d12fbeabf131
sertac.guler <sertac8006@gmail.com>
2019-02-16 11:09:46 +0300

    Odev dizideki karışık haldeki sayıları sıralama

---
+++ Odev/src/OdevSirala.java
@@ -0,0 +1,24 @@
+public class OdevSirala {
+
+    public static void main(String[] args) {
+        int[] dizi = {7, 5, 4, 6, 3, 1, 10, 8, 9, 2};
+        int temp = 0;
+        for (int i=0; i<dizi.length; i++) {
+            for (int x=i+1; x<dizi.length; x++) {
+                if (dizi[i] > dizi[x])
+                {
+                    temp = dizi[i];
+                    dizi[i] = dizi[x];
+                    dizi[x] = temp;
+                }
+            }
+            //System.out.println(dizi[i]);
+        }
+        String sonuc= "";
+        for (int i : dizi) {
+                    sonuc += i + " ";
+        }
+        System.out.println("Dizi'nin sıralanmış hali : " + sonuc + " ***");
+    }
+
+}
