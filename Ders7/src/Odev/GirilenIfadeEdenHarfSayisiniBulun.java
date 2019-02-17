70e4b4f8e3c1f3c62a92a6ebf6df7cd467facbab
sertac.guler <sertac8006@gmail.com>
2019-02-17 13:41:45 +0300

    ödev

---
+++ Ders7/src/Odev/GirilenIfadeEdenHarfSayisiniBulun.java
@@ -0,0 +1,14 @@
+package Odev;
+
+public class GirilenIfadeEdenHarfSayisiniBulun {
+    public static void main(String[] args) {
+        /*
+        * Kullanıcı
+        * Önce cümle yada kelime girecek
+        * Sonrasında bir harf girecek
+        * program kullanıcının girdigi harf sayısını girilen ifade içinde bulacak
+        * */
+    }
+
+
+}
