532c6407780f3dd8c08f62bfd0571c1cf7e125c3
sertac.guler <sertac8006@gmail.com>
2019-02-17 13:53:25 +0300

    ödev

---
+++ Ders7/src/Odev/DiziOdev.java
@@ -0,0 +1,19 @@
+package Odev;
+
+public class DiziOdev {
+
+    public static void main(String[] args) {
+        /*
+        //kullanıcı dizinin boyutunu girecek ve merdiven şeklinde aşağıdaki şekil oluşturulacak
+        // satır ve sutun eşit olcak.
+        //dizi[i][k] = "x"
+        // 4 x 4 bir dizi alttaki şekil
+        // dizi ile yapılacak
+        *
+       **
+      ***
+     ****
+
+         */
+    }
+}
