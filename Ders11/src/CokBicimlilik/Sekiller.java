3b571f4560e30dc9ecb4e766533374869d6c90a6
sertac.guler <sertac8006@gmail.com>
2019-03-03 12:09:35 +0300

    polimrofiz yaotık

---
+++ Ders11/src/CokBicimlilik/Sekiller.java
@@ -0,0 +1,15 @@
+package CokBicimlilik;
+
+public class Sekiller {
+
+    public double alanHesapla() {
+
+        return 0;
+    }
+
+    public double cevreHesapla() { // method final ise override yapamayız
+
+        return 0;
+    }
+
+}
